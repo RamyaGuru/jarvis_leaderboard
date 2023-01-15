64da16fa6fc5121c5d229bf99777b1203dab9233
knc6 <writetokamal.1989@gmail.com>
2023-01-14 22:15:30 -0500

    Metadata update.

--- docs/AI/exfoliation_energy.md
+++ docs/AI/exfoliation_energy.md
@@ -1,27 +1,21 @@
 # Model for exfoliation energy
-This is a template repository for anyone that wants to use the MkDocs Material Theme.
+This is a template repository for JARVIS-Leaderboard benchmarks.
 
-
-<h2>A basic HTML table</h2>
+<h2>Model benchmarks</h2>
 
 <table style="width:100%" id="j_table">
  <thead>
   <tr>
     <th>Team name</th>
-    <th>Model</th>
+    <th>Method</th>
     <th>MAE</th>
+    <th>Model name</th>
+    <th>Team name</th>
     <th>Dataset size</th>
     <th>Date submitted</th>
   </tr>
  </thead>
 <!--table_content-->
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>3</td>
-    <td>MAE</td>
-    <td>3</td>
-  </tr>
 </table>
 
 
--- jarvis_leaderboard/rebuild.py
+++ jarvis_leaderboard/rebuild.py
@@ -9,6 +9,43 @@ print("Running modify.py script")
 root_dir = os.path.dirname(os.path.abspath(__file__))
 clean = True
 
+def get_metric_value(
+    csv_path="", dataset="", prop="", data_split="", method="", metric=""
+):
+    results={}
+    csv_data = pd.read_csv(csv_path)
+    meta_path=csv_path.split('/')
+    meta_path[-1]='metadata.json'
+    meta_path='/'.join(meta_path)
+    meta_data=loadjson(meta_path)
+    results['model_name']=meta_data['model_name']
+    results['team_name']=meta_data['team_name']
+    results['date_submitted']=meta_data['date_submitted']
+    
+    print('meta_path',meta_data)
+    #meta_data=loadjson()
+    print ('csv_data',csv_path)
+    temp = dataset + "_" + prop + ".json"
+    fname = os.path.join("dataset", method, temp)
+    json_data = loadjson(os.path.join(root_dir, fname))
+    actual_data_json = json_data[data_split]
+    data_size=len(json_data['train'])+len(json_data['val'])+len(json_data['test'])
+    # print ('actual_data_json',actual_data_json)
+    results['dataset_size']=data_size
+    ids = []
+    targets = []
+    for i, j in actual_data_json.items():
+        ids.append(i)
+        targets.append(j)
+    mem = {"id": ids, "actual": targets}
+    actual_df = pd.DataFrame(mem)
+    df = pd.merge(csv_data, actual_df, on="id")
+    results['res']='na'
+    if metric == "mae":
+        res = round(mean_absolute_error(df["actual"], df["prediction"]),2)
+        results['res']=res
+    return results
+
 for i in glob.glob("jarvis_leaderboard/benchmarks/*/*.csv"):
     fname = i.split("/")[-1].split(".csv")[0]
     temp = fname.split("-")
@@ -20,17 +57,17 @@ for i in glob.glob("jarvis_leaderboard/benchmarks/*/*.csv"):
     team = i.split("/")[-2]
     md_filename = "../docs/" + method + "/" + prop + ".md"
     md_path = os.path.join(root_dir, md_filename)
-    print(
-        fname,
-        data_split,
-        prop,
-        dataset,
-        method,
-        metric,
-        team,
-        md_filename,
-        md_path,
-    )
+    #print(
+    #    fname,
+    #    data_split,
+    #    prop,
+    #    dataset,
+    #    method,
+    #    metric,
+    #    team,
+    #    md_filename,
+    #    md_path,
+    #)
     with open(md_path, "r") as file:
         filedata = file.read().splitlines()
     content = []
@@ -42,28 +79,6 @@ for i in glob.glob("jarvis_leaderboard/benchmarks/*/*.csv"):
     with open(md_path, "w") as file:
         file.write("\n".join(content))
 # jarvis_leaderboard/dataset/AI/dft_3d_exfoliation_energy.json
-def get_metric_value(
-    csv_path="", dataset="", prop="", data_split="", method="", metric=""
-):
-
-    csv_data = pd.read_csv(csv_path)
-    # print ('csv_data',csv_data)
-    temp = dataset + "_" + prop + ".json"
-    fname = os.path.join("dataset", method, temp)
-    actual_data_json = loadjson(os.path.join(root_dir, fname))[data_split]
-    # print ('actual_data_json',actual_data_json)
-    ids = []
-    targets = []
-    for i, j in actual_data_json.items():
-        ids.append(i)
-        targets.append(j)
-    mem = {"id": ids, "actual": targets}
-    actual_df = pd.DataFrame(mem)
-    df = pd.merge(csv_data, actual_df, on="id")
-    if metric == "mae":
-        res = mean_absolute_error(df["actual"], df["prediction"])
-    return res
-
 
 for i in glob.glob("jarvis_leaderboard/benchmarks/*/*.csv"):
     fname = i.split("/")[-1].split(".csv")[0]
@@ -111,13 +126,19 @@ for i in glob.glob("jarvis_leaderboard/benchmarks/*/*.csv"):
         + method
         + "</td>"
         + "<td>"
-        + str(res)
+        + str(res['res'])
         + "</td>"
         + "<td>"
-        + str(res)
+        + str(res['model_name'])
         + "</td>"
         + "<td>"
-        + str(res)
+        + str(res['team_name'])
+        + "</td>"
+        + "<td>"
+        + str(res['dataset_size'])
+        + "</td>"
+        + "<td>"
+        + str(res['date_submitted'])
         + "</td>"
         + "</tr>"
     )
